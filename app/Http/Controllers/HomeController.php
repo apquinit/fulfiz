bddc713fd7b1275e4b129e52d7779f06cab2e33b
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-08 12:07:44 +0800

    Remove callback function in route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,5 +14,8 @@ class ExampleController extends Controller
         //
     }
 
-    //
+    public function getAppName()
+    {
+        return 'Irene Fulfillment Server (0.0.0)';
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
 $router->get('/', function () use ($router) {
     return 'Irene Fulfillment Server (0.0.0)';
 });
+
+$router->get('/', ['uses' => 'HomeController@getAppName']);
