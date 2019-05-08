63665b94543b97c7b54444ad868eeff945f40f0c
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-08 15:19:27 +0800

    Register database configuration file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,8 @@ $app->withEloquent();
 
 $app->configure('app');
 
+$app->configure('database');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
