8e85b83221bb751d394c64bcb57c9287f999fdae
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-10 10:27:12 +0800

    Add webhook controller

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class WebhookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle()
+    {
+        //
+    }
+}
