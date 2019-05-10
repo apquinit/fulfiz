460979e1500039ae9deb8c05a14fe4656c37ce0f
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-10 15:51:21 +0800

    Add service

--- app/Services/Action/WeatherService.php
+++ app/Services/Action/WeatherService.php
@@ -20,4 +20,32 @@ class WeatherService
         $this->locationIqService = new LocationIQService;
         $this->darkSkyService = new DarkSkyService;
     }
+
+    public function actionCurrentWeather($cityName)
+    {
+        // 1. Get latitude and longitude from Location IQ Service by city name.
+        $location = $this->getLatitudeAndLongitudeFromLocationService($cityName);
+        $latitude = $location['lat'];
+        $longitude = $location['lon'];
+
+        // 2. Get weather data from Dark Sky Service by latitude and longitude
+        // 3. Assemble text response from weather data.
+
+        $textResponse = 'The weather forecast calls for...';
+    }
+
+    public function actionWeatherByDate($cityName, $date)
+    {
+        // 1. Get latitude and longitude from Location IQ Service by city name.
+        // 2. Get weather data from Dark Sky Service by latitude, longitude and date.
+        // 3. Assemble text response from weather data.
+
+        $textResponse = 'The weather forecast calls for...';
+    }
+
+    private function getLatitudeAndLongitudeFromLocationService($cityName)
+    {
+        return $this->locationIqService->getLatitudeAndLongitude($cityName);
+    }
+
 }
