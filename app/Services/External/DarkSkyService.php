42c0d8f952ae03afa40057c0f0aedf88de2d3485
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-10 15:02:34 +0800

    Add get weather by date function

--- app/Services/External/DarkSkyService.php
+++ app/Services/External/DarkSkyService.php
@@ -31,4 +31,17 @@ class DarkSkyService
         
         return $currentWeather;
     }
+
+    public function getWeatherByDate($latitude, $longitude, $date)
+    {
+        // Dark Sky forecast request URL (https://api.darksky.net/forecast/5a050170535218d28b85e8cad4e6f781/14.5906216,120.9799696,2019-05-10T12:00:00+08:00?exclude=[minutely,hourly,daily,alerts,flags])
+
+        $exclude = '[minutely,hourly,daily,alerts,flags]';
+        $requestUrl = config('api.dark_sky.base_url') . '/' . config('api.dark_sky.api_key') . '/' . $latitude . ',' . $longitude . ',' . $date . '?exclude=' . $exclude;
+        $response  = $this->guzzleClient->get($requestUrl);
+        $content = json_decode($response->getBody()->getContents(), true);
+        $currentWeather = $content['currently'];
+        
+        return $currentWeather;
+    }
 }
