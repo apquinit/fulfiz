8730bb9a97eddd1a573960bbe87942dde2902bd4
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-15 07:35:02 +0800

    Fix config file

--- config/jwt.php
+++ config/jwt.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'lifetime' => env('JWT_LIFETIME', 60),
+    'lifetime' => env('JWT_LIFETIME', getenv('JWT_LIFETIME') ? null : 60),
     'key' => env('JWT_KEY', getenv('JWT_KEY') ? null : null),
 ];
