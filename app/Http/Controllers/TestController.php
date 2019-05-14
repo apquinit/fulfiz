66ee5d20b232f1ea9a5d127edbe8271d27f7c21d
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-14 11:03:36 +0800

    Remove comments in test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,21 +16,6 @@ class TestController extends Controller
 
     public function test()
     {
-        // Dark Sky Current
-        // return (new \App\Services\External\DarkSkyService)->getCurrentWeather('14.5906216', '120.9799696');
-
-        // Dark Sky Date
-        // return (new \App\Services\External\DarkSkyService)->getWeatherByDate('14.5906216', '120.9799696', '2019-05-10T12:00:00+08:00');
-
-        // Location IQ
-        // return (new \App\Services\External\LocationIQService)->getLatitudeAndLongitude('Manila');
-
-        // Current Weather Action Service
-        // return (new \App\Services\Action\WeatherService)->getCurrentWeatherTextResponse('Manila');
-
-        // Weather by Date Action Service
-        // return (new \App\Services\Action\WeatherService)->getWeatherByDateTextResponse('Manila', '2019-05-14T12:00:00+08:00');
-
         return 'Test Endpoint';
     }
 }
