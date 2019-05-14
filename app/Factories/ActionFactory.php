5d52b851d5a389072c164f73cccc93f965c954bc
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-14 11:28:02 +0800

    Map weather service

--- app/Factories/ActionFactory.php
+++ app/Factories/ActionFactory.php
@@ -2,12 +2,19 @@
 
 namespace App\Factories;
 
-use App\Factories\ActionMapperFactory;
+use Illuminate\Http\Request;
+use App\Services\Action\CurrentWeatherService;
+use App\Services\Action\WeatherByDateService;
 
 class ActionFactory
 {
-    public function mapActionToService(string $action)
+    public function mapActionToService(Request $request)
     {
-        // if
+        if ($request['queryResult']['action'] == 'weather.current') {
+            return new CurrentWeatherService($request['queryResult']['parameters']['geo-city']);
+        }
+        else if ($request['queryResult']['action'] == 'weather.date') {
+            return new WeatherByDateService($request['queryResult']['parameters']['geo-city'], $request['queryResult']['parameters']['date']);
+        }
     }
 }
