a043b528a7cb6f90ba8314dc095bac47c7c42dcc
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-05-15 08:37:12 +0800

    Create DuckDuckGo service

---
+++ app/Services/External/DuckDuckGoService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services\External;
+
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class DuckDuckGoService
+{
+    private $guzzleClient;
+
+    /**
+     * Create a new service instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->guzzleClient = new Client;
+    }
+
+    public function getInstantAnswer($topic)
+    {
+        // DuckDuckGo Instant Answer request URL (https://api.duckduckgo.com/?q=artificial%20intelligence&format=json&no_redirect=1&no_html=1&skip_disambig=1)
+
+        $requestUrl = config('api.duck_duck_go.base_url') . '/?q=' . $topic  . '&format=json&no_redirect=1&no_html=1&skip_disambig=1';
+        $response  = $this->guzzleClient->get($requestUrl);
+        $content = json_decode($response->getBody()->getContents(), true);
+
+        return $content;
+    }
+}
