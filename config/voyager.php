fbc5d0a7dc70ca3f1a96d26f00b9472618441735
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-21 18:41:57 +0800

    Change primary color

--- config/voyager.php
+++ config/voyager.php
@@ -186,7 +186,7 @@ return [
     |
     */
 
-    'primary_color' => '#2D1220',
+    'primary_color' => '#464675',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
