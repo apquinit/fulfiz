0ed1a8673b980c9a1f881a9241962d4fcd80f816
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-24 08:22:42 +0800

    Create key model

---
+++ database/migrations/2019_06_24_002039_create_keys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->text('description');
+            $table->text('status');
+            $table->text('key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keys');
+    }
+}
