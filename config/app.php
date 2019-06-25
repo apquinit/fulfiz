3046b96da4275eeacb97bff4b21bfbfd8d0cbe52
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-25 11:18:28 +0800

    Add alias to request helper

--- config/app.php
+++ config/app.php
@@ -217,6 +217,7 @@ return [
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         'Redis' => Illuminate\Support\Facades\Redis::class,
         'Request' => Illuminate\Support\Facades\Request::class,
+        'RequestHelper' => App\Helpers\RequestHelper::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
