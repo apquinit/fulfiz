85bed9f24b144edb83e7c4f230606e83855f72c0
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-25 12:32:37 +0800

    Create date time current service

---
+++ app/Services/Dialogflow/DateTimeCurrentFulfillmentService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services\Dialogflow;
+
+use App\Interfaces\DialogflowFulfillmentInterface;
+
+/**
+ * Class KeyRepository.
+ */
+class DateTimeCurrentFulfillmentService extends DialogflowFulfillmentService implements DialogflowFulfillmentInterface
+{
+    protected $parameters;
+
+    public function __construct()
+    {
+
+    }
+
+    public function setParameters(array $parameters) : void
+    {
+
+    }
+
+    public function getTextResponse() : string
+    {
+
+    }
+
+    public function process() : void
+    {
+        
+    }
+}
