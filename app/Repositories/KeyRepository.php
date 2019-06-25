3713f22cdfb220879cd2cd0c0587e76ea59ad57e
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-25 08:47:04 +0800

    Create key repository

---
+++ app/Repositories/KeyRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use NamTran\LaravelMakeRepositoryService\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class KeyRepository.
+ */
+class KeyRepository extends BaseRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        //return YourModel::class;
+    }
+}
