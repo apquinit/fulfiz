75ef097a262e59ae804c516360b1008b87f9a8b1
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-25 09:58:01 +0800

    Bind interface to service

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,16 +7,22 @@ use Illuminate\Support\ServiceProvider;
 class RepositoryServiceProvider extends ServiceProvider
 {
     /**
+     * All of the container bindings that should be registered.
+     *
+     * @var array
+     */
+    public $bindings = [
+        \App\Interfaces\KeyRepositoryInterface::class => \App\Repositories\KeyRepository::class,
+    ];
+
+    /**
      * Register services.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->bind(
-            'App\Interfaces\KeyRepositoryInterface',
-            'App\Repositories\KeyRepository'
-        );
+        //
     }
 
     /**
