d4eb007ff7406aed00151203d412e825985973c5
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-25 15:00:38 +0800

    Create fulfillment controller

--- app/Http/Controllers/DialogflowFulfillmentController.php
+++ app/Http/Controllers/DialogflowFulfillmentController.php
@@ -2,13 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use Log;
 use Illuminate\Http\Request;
 use Dialogflow\WebhookClient;
+use App\Interfaces\DialogflowFulfillmentInterface;
+
+use App\Helpers\TimeZoneDbHelper;
 
 class DialogflowFulfillmentController extends Controller
 {
+    private $request;
+    private $agent;
+    private $session;
+    private $query;
+    private $intent;
+    private $action;
+    private $parameters;
+    private $fulfillmentService;
+
+    public function __construct(Request $request, DialogflowFulfillmentInterface $fulfillmentService)
+    {
+        $this->request = $request;
+        $this->fulfillmentService = $fulfillmentService;
+    }
+
     public function handle()
     {
-        return 'Fulfillment';
+        dd(get_current_date_time(14.5906216, 120.9799696));
+
+        // Instantiate a Dialogflow Webhook client from the request
+        $this->agent = WebhookClient::fromData($this->request->json()->all());
+
+        // Store values to properties
+        $this->session = $this->agent->getSession();
+        $this->query = $this->agent->getQuery();
+        $this->intent = $this->agent->getIntent();
+        $this->action = $this->agent->getAction();
+        $this->parameters = $this->agent->getParameters();
+
+        // Get session/device ID from request
+        // Get user based on the session/device ID
+
+        // Pass parameters array to setParameters() method of $dialogflowFulfillmentService
+        $this->fulfillmentService->setParameters($this->parameters);
+
+        // Call process() method in $dialogflowFulfillmentService to execute action and generate a text response
+        $this->fulfillmentService->process();
+
+        // Call getTextResponse() method of $dialogflowFulfillmentService to get generated text response property
+        $textResponse = $this->fulfillmentService->getTextResponse();
+
+        // Pass text response to agent->reply() method
+        $this->agent->reply($textResponse);
+
+        Log::info('Dialogflow request', [
+            'Session' => $this->session,
+            'Query' => $this->query,
+            'Intent' => $this->intent,
+            'Action' => $this->action,
+            'Parameters' => $this->parameters,
+            'Response' => $agent->render()
+            ]
+        );
+
+        // Return response to Dialogflow
+        return response()->json($agent->render());
     }
 }
