6b11bbe98cf473a133f79d2ad07d83c5ffc57d2a
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-27 12:48:47 +0800

    Update middleware

--- app/Http/Middleware/BindSessionToDevice.php
+++ app/Http/Middleware/BindSessionToDevice.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Log;
 
 class BindSessionToDevice
 {
@@ -15,6 +16,8 @@ class BindSessionToDevice
      */
     public function handle($request, Closure $next)
     {
+        Log::info('Bind session to device', ['Session' => $request->session]);
+
         return $next($request);
     }
 }
