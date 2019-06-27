39d286822abd6f32b8f47072dfa8cd7aa77056f6
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-27 14:37:09 +0800

    Create location iq user model

--- app/Helpers/LocationIqHelper.php
+++ app/Helpers/LocationIqHelper.php
@@ -12,13 +12,25 @@ if (!function_exists('get_latitude_and_longitude')) {
      * @param
      * @return
      */
-    function get_latitude_and_longitude(string $city) : array
+    function get_latitude_and_longitude(string $userId, string $city) : array
     {
         // Location IQ request URL (https://us1.locationiq.com/v1/search.php?key=YOUR_PRIVATE_TOKEN&q=SEARCH_STRING&format=json)
 
         $guzzleClient = new Client;
 
-        $requestUrl = config('services.location_iq.base_url') . '?key=' . config('services.location_iq.api_key') . '&q=' . $city . '&format=json';
+        if (strpos($userId, 'irene-lite-vbvypr') !== false or strpos($userId, 'irene-4fe98') !== false) {
+            $key = config('services.location_iq.api_key');
+        } else {
+            $locationIqUser = $locationIqUsersRepository->getByUserId($userId);
+
+            if ($locationIqUser->status === 'DISABLED') {
+                abort(401, 'Unauthorized');
+            } else {
+                $key = $locationIqUser->token;
+            }
+        }
+
+        $requestUrl = config('services.location_iq.base_url') . '?key=' . $key . '&q=' . $city . '&format=json';
         $response  = $guzzleClient->get($requestUrl);
         $content = json_decode($response->getBody()->getContents(), true);
         $location = [
