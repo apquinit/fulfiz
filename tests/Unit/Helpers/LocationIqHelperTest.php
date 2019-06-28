57ea8c8ee051698bf20bc44f675ed761e36b1be4
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-28 17:41:20 +0800

    Create location iq helper tester

--- tests/Unit/Helpers/LocationIqHelperTest.php
+++ tests/Unit/Helpers/LocationIqHelperTest.php
@@ -12,23 +12,43 @@ class LocationIqHelperTest extends TestCase
 
     public function setUp() : void
     {
-        parent::setup();
+        parent::setUp();
+
+        config(['app.dialogflow.irene_lite' => 'irene-lite-vbvypr']);
+        config(['app.dialogflow.irene' => 'irene-4fe98']);
+        config(['services.location_iq.base_url' => 'https://us1.locationiq.com/v1/search.php']);
+        config(['services.location_iq.api_key' => '4c53f82e708066']);
     }
 
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function get_latitude_and_longitude_method_shoud_return_latitude_and_longitude_when_passed_dialogflow_agent_id()
     {
-        // Create operator
+        $location = get_latitude_and_longitude('dialogflow/agent/irene-lite-vbvypr', 'Manila');
+
+        $this->assertArrayHasKey('lat', $location);
+        $this->assertArrayHasKey('lon', $location);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function get_latitude_and_longitude_method_shoud_return_latitude_and_longitude_when_passed_user_id()
+    {
         factory(\App\Models\LocationIqUser::class)->create(
             [
-                'user_id' => 1
+                'user_id' => 1,
+                'token' => '4c53f82e708066',
+                'status' => 'ENABLED'
             ]
         );
 
+        $location = get_latitude_and_longitude('1', 'Manila');
+
+        $this->assertArrayHasKey('lat', $location);
+        $this->assertArrayHasKey('lon', $location);
         $this->assertTrue(true);
     }
 }
