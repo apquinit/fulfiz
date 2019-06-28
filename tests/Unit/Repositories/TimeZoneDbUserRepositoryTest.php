559e4a393985665332d12ff57c32cc36a8615fae
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-06-28 18:32:49 +0800

    Create time zone db user repository test

---
+++ tests/Unit/Repositories/TimeZoneDbUserRepositoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Repositories\TimeZoneDbUserRepository;
+
+class TimeZoneDbUserRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->timeZoneDbUserRepository = new TimeZoneDbUserRepository;
+    }
+
+    /**
+     * @test
+     */
+    public function getByUserId_method_should_return_time_zone_db_user_object_when_passed_user_id()
+    {
+        factory(\App\Models\TimeZoneDbUser::class)->create(
+            [
+                'user_id' => 1,
+            ]
+        );
+
+        $timeZoneDbUser = $this->timeZoneDbUserRepository->getByUserId(1);
+
+        $this->assertTrue($timeZoneDbUser instanceof \App\Models\TimeZoneDbUser);
+    }
+}
