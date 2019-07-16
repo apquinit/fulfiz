a89e89d96a32da2d5c1fd124a1c9632f92195fc5
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-07-16 16:06:50 +0800

    Add get date weather function

--- tests/Helpers/DarkSkyHelperTest.php
+++ tests/Helpers/DarkSkyHelperTest.php
@@ -60,4 +60,52 @@ class DarkSkyHelperTest extends TestCase
         $this->assertArrayHasKey('pressure', $currentWeather);
         $this->assertArrayHasKey('nearestStormDistance', $currentWeather);
     }
+
+    /**
+     * @test
+     */
+    public function get_date_weather_function_should_return_date_weather_when_passed_dialogflow_agent_id_and_latitude_longitude_and_date()
+    {
+        $dateWeather = get_date_weather('dialogflow/agent/irene-lite-vbvypr', 14.5906216, 120.9799696, '2019-07-17T12:00:00+08:00');
+
+        $this->assertArrayHasKey('summary', $dateWeather);
+        $this->assertArrayHasKey('temperatureMin', $dateWeather);
+        $this->assertArrayHasKey('temperatureMax', $dateWeather);
+        $this->assertArrayHasKey('apparentTemperatureMin', $dateWeather);
+        $this->assertArrayHasKey('apparentTemperatureMax', $dateWeather);
+        $this->assertArrayHasKey('precipProbability', $dateWeather);
+        $this->assertArrayHasKey('precipIntensity', $dateWeather);
+        $this->assertArrayHasKey('humidity', $dateWeather);
+        $this->assertArrayHasKey('windSpeed', $dateWeather);
+        $this->assertArrayHasKey('windGust', $dateWeather);
+        $this->assertArrayHasKey('pressure', $dateWeather);
+    }
+
+    /**
+     * @test
+     */
+    public function get_date_weather_function_should_return_date_weather_when_passed_user_id_and_latitude_longitude_and_date()
+    {
+        factory(\App\Models\DarkSkyUser::class)->create(
+            [
+                'user_id' => 1,
+                'token' => '5a050170535218d28b85e8cad4e6f781',
+                'status' => 'ENABLED'
+            ]
+        );
+
+        $dateWeather = get_date_weather('dialogflow/agent/irene-lite-vbvypr', 14.5906216, 120.9799696, '2019-07-17T12:00:00+08:00');
+
+        $this->assertArrayHasKey('summary', $dateWeather);
+        $this->assertArrayHasKey('temperatureMin', $dateWeather);
+        $this->assertArrayHasKey('temperatureMax', $dateWeather);
+        $this->assertArrayHasKey('apparentTemperatureMin', $dateWeather);
+        $this->assertArrayHasKey('apparentTemperatureMax', $dateWeather);
+        $this->assertArrayHasKey('precipProbability', $dateWeather);
+        $this->assertArrayHasKey('precipIntensity', $dateWeather);
+        $this->assertArrayHasKey('humidity', $dateWeather);
+        $this->assertArrayHasKey('windSpeed', $dateWeather);
+        $this->assertArrayHasKey('windGust', $dateWeather);
+        $this->assertArrayHasKey('pressure', $dateWeather);
+    }
 }
