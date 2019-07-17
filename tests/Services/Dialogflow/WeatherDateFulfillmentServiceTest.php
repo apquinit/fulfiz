901239223309cdf739f5fe7cbe06aca369489129
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-07-17 09:31:48 +0800

    Add weather date fulfillment service test

---
+++ tests/Services/Dialogflow/WeatherDateFulfillmentServiceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Services\Dialogflow\WeatherDateFulfillmentService;
+
+class WeatherDateFulfillmentServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->weatherDateFulfillmentService = new WeatherDateFulfillmentService;
+    }
+
+    /**
+     * @test
+     */
+    public function WeatherDateFulfillmentService_class_should_exist()
+    {
+        $this->assertTrue(class_exists(WeatherDateFulfillmentService::class));
+    }
+
+    /**
+     * @test
+     */
+    public function WeatherDateFulfillmentService_class_should_return_text_response_when_passed_user_and_parameters_array()
+    {
+        $user = [
+            'id' => 'dialogflow/agent/irene-lite-vbvypr'
+        ];
+
+        $parameters = [
+            'city' => 'Manila',
+            'date' => '2019-07-17T12:00:00+08:00'
+        ];
+
+        $this->weatherDateFulfillmentService->setParameters($user, $parameters);
+        $this->weatherDateFulfillmentService->process();
+        $textResponse = $this->weatherDateFulfillmentService->getTextResponse();
+
+        $this->assertTrue(is_string($textResponse));
+    }
+}
