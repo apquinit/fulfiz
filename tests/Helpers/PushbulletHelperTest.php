60c799763bcac12fa656fa60132c2c4e90d54574
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-07-17 13:39:58 +0800

    Create Pushbullet helper

---
+++ tests/Helpers/PushbulletHelperTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PushbulletHelperTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    public function push_note_to_channel_should_return_response_code_when_passed_channel_and_title_and_message()
+    {
+        $responseCode = push_note_to_channel('irene-test', 'Unit Test', 'This is a test message.');
+        
+        dd($responseCode);
+
+        $this->assertArrayHasKey('AbstractText', $instantAnswer);
+    }
+}
