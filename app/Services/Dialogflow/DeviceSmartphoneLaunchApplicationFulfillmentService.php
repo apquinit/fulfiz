d246037513e8e5ce591ddfdf3042399959700581
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-07-17 15:58:39 +0800

    Create device smartphone launch application fulfillment service

---
+++ app/Services/Dialogflow/DeviceSmartphoneLaunchApplicationFulfillmentService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services\Dialogflow;
+
+use App\Interfaces\DialogflowFulfillmentServiceInterface;
+use App\Repositories\DeviceRepository;
+
+class DeviceSmartphoneLaunchApplicationFulfillmentService extends DialogflowFulfillmentService implements DialogflowFulfillmentServiceInterface
+{
+    public function setParameters(array $user, array $parameters) : void
+    {
+        $this->user = $user;
+        $this->parameters = $parameters;
+    }
+
+    public function getTextResponse() : string
+    {
+        return $this->textResponse;
+    }
+
+    public function process() : void
+    { 
+        $responseCode = send_autoremote_message($this->user['device_code'], 'LAUNCH ' . strtoupper($this->parameters['application']));
+
+        if ($responseCode === 200) {
+            $textResponseArray = ['Okay.', 'Sure!', 'Got it.', 'Opening application.', 'Okay. Opening application.', 'Sure! Opening application.', 'Got it. Opening application.'];
+            $textResponseIndex = array_rand($textResponseArray);
+            $this->textResponse = $textResponseArray[$textResponseIndex];
+        } else {
+            $this->textResponse = 'An error occured while I\'m trying to access your smartphone.';
+        }
+
+        return;
+    }
+}
