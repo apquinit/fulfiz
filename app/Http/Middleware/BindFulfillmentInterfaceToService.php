4960ec7fa1778050162076584f1eb95d066cf034
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-08-09 14:03:29 +0800

    Add play random music on Spotify fulfillment

--- app/Http/Middleware/BindFulfillmentInterfaceToService.php
+++ app/Http/Middleware/BindFulfillmentInterfaceToService.php
@@ -28,6 +28,8 @@ class BindFulfillmentInterfaceToService
                         return new \App\Services\Dialogflow\DateTimeCurrentFulfillmentService;
                     } elseif ($agent->getAction() === 'device.smartphone.launch_application') {
                         return new \App\Services\Dialogflow\DeviceSmartphoneLaunchApplicationFulfillmentService;
+                    } elseif ($agent->getAction() === 'device.smartphone.play_random_music_on_spotify') {
+                        return new \App\Services\Dialogflow\DeviceSmartphonePlayRandomMusicOnSpotifyFulfillmentService;
                     } elseif ($agent->getAction() === 'weather.current') {
                         return new \App\Services\Dialogflow\WeatherCurrentFulfillmentService;
                     } elseif ($agent->getAction() === 'weather.date') {
