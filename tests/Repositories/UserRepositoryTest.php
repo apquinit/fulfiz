e7dc3ca9ab352f9da60b5c76c0ea53bb58cef8a2
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-08-14 17:38:32 +0800

    Create user repository

---
+++ tests/Repositories/UserRepositoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Repositories\UserRepository;
+
+class UserRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->userRepository = new UserRepository;
+    }
+
+    /**
+     * @test
+     */
+    public function UserRepository_class_should_exist()
+    {
+        $this->assertTrue(class_exists(UserRepository::class));
+    }
+
+    /**
+     * @test
+     */
+    public function getByUserId_method_should_return_user_object_when_passed_user_id()
+    {
+        factory(\App\Models\User::class)->create(
+            [
+                'id' => 1,
+            ]
+        );
+
+        $user = $this->userRepository->getByUserId(1);
+
+        $this->assertTrue($user instanceof \App\Models\User);
+    }
+}
