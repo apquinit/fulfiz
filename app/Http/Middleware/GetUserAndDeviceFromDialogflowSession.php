ed67b140831bad5e849893d08944e1074bc77c6a
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-09-13 15:07:29 +0800

    Update log info message

--- app/Http/Middleware/GetUserAndDeviceFromDialogflowSession.php
+++ app/Http/Middleware/GetUserAndDeviceFromDialogflowSession.php
@@ -29,17 +29,17 @@ class GetUserAndDeviceFromDialogflowSession
         if (strpos($session, config('app.dialogflow.irene_lite')) !== false) {
             $user = config('app.dialogflow.irene_lite');
             $code = 'irene-lite-messenger';
-            Log::info('Bind session to device', ['Session' => $session, 'Device' => 'Irene Lite Messenger']);
+            Log::info('Get user and device from session', ['Session' => $session, 'Device' => 'Irene Lite Messenger']);
         } elseif (strpos($session, config('app.dialogflow.irene')) !== false) {
             $user = config('app.dialogflow.irene');
             $code = 'irene-messenger';
-            Log::info('Bind session to device', ['Session' => $session, 'Device' => 'Irene Messenger']);
+            Log::info('Get user and device from session', ['Session' => $session, 'Device' => 'Irene Messenger']);
         } else {
             $device = $this->deviceRepository->getByCode($session);
             if ($device->status === 'ENABLED') {
                 $user = $device->user_id;
                 $code = $device->code;
-                Log::info('Bind session to device', ['Session' => $session, 'Device' => $device->name]);
+                Log::info('Get user and device from session', ['Session' => $session, 'Device' => $device->name]);
             } elseif ($device->status === 'DISABLED') {
                 abort(403, 'Device disabled.');
             } else {
