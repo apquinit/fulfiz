83d89fca61c686bf08f03620856f05f84a0a025c
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-09-16 18:55:19 +0800

    Update middleware

--- app/Http/Middleware/GetUserAndDeviceFromDialogflowSession.php
+++ app/Http/Middleware/GetUserAndDeviceFromDialogflowSession.php
@@ -5,13 +5,16 @@ namespace App\Http\Middleware;
 use Closure;
 use Log;
 use App\Repositories\DeviceRepository;
+use App\Repositories\UserRepository;
 
 class GetUserAndDeviceFromDialogflowSession
 {
+    private $userRepository;
     private $deviceRepository;
 
-    public function __construct(DeviceRepository $deviceRepository)
+    public function __construct(UserRepository $userRepository,DeviceRepository $deviceRepository)
     {
+        $this->userRepository = $userRepository;
         $this->deviceRepository = $deviceRepository;
     }
 
@@ -26,20 +29,29 @@ class GetUserAndDeviceFromDialogflowSession
     {
         $session = $request->agent->getSession();
 
-        if (strpos($session, config('app.dialogflow.irene_lite')) !== false) {
-            $user = config('app.dialogflow.irene_lite');
-            $code = 'irene-lite-messenger';
-            Log::info('Get user and device from session', ['Session' => $session, 'Device' => 'Irene Lite Messenger']);
-        } elseif (strpos($session, config('app.dialogflow.irene')) !== false) {
-            $user = config('app.dialogflow.irene');
-            $code = 'irene-messenger';
-            Log::info('Get user and device from session', ['Session' => $session, 'Device' => 'Irene Messenger']);
+        if (isset($request['originalDetectIntentRequest']['payload']['source']) and $request['originalDetectIntentRequest']['payload']['source'] === 'facebook') {
+            if (strpos($session, config('app.dialogflow.irene_lite')) !== false) {
+                $userId = config('app.dialogflow.irene_lite');
+                $deviceCode = 'irene-lite-messenger';
+                Log::info('Get user and device from session', ['Session' => $session, 'Source' => 'Facebook', 'User' => $userId, 'Device' => $deviceCode]);
+            } elseif (strpos($session, config('app.dialogflow.irene')) !== false) {
+                // Get Facebook user PSID
+                $psid = $request['originalDetectIntentRequest']['payload']['data']['sender']['id'];
+                $userName = get_facebook_graph_user_name($psid);
+                // Get User
+                $user = $this->userRepository->getByUserName($userName);
+                $userId = $user->id;
+                // Get Device
+                $device = $this->deviceRepository->getByUserId($user->id);
+                $deviceCode = $device->code;
+                Log::info('Get user and device from session', ['Session' => $session, 'Source' => 'Facebook', 'User' => $userId, 'Device' => $deviceCode]);
+            }
         } else {
             $device = $this->deviceRepository->getByCode($session);
             if ($device->status === 'ENABLED') {
-                $user = $device->user_id;
-                $code = $device->code;
-                Log::info('Get user and device from session', ['Session' => $session, 'Device' => $device->name]);
+                $userId = $device->user_id;
+                $deviceCode = $device->code;
+                Log::info('Get user and device from session', ['Session' => $session, 'Source' => $device->name, 'User' => $userId, 'Device' => $deviceCode]);
             } elseif ($device->status === 'DISABLED') {
                 abort(403, 'Device disabled.');
             } else {
@@ -48,8 +60,8 @@ class GetUserAndDeviceFromDialogflowSession
         }
 
         $request->user = [
-            'id' => $user,
-            'device_code' => $code
+            'id' => $userId,
+            'device_code' => $deviceCode
         ];
 
         return $next($request);
